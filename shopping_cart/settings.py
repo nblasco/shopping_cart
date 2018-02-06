6c7a395652ec8eda11e3d4e8441dcd32e3e1f988
Niriana Blasco <nblasco@konoopo.com>
2018-02-06 14:13:12 -0400

    create app cart

--- shopping_cart/settings.py
+++ shopping_cart/settings.py
@@ -37,14 +37,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
+
+THIRD_PARTY_APPS = ()
+
+LOCAL_APPS = (
+    'cart',
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
